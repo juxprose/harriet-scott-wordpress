a8a90b65242fe3c3766c3514d3db03ac4f96e4f5
David Flindall <david@curiousways.com>
2021-02-28 14:07:21 +0000

    new styles
    Signed-off-by: David Flindall <david@curiousways.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,26 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+     colors: {
+       'hs-maroon': '#800000',
+      },
       fontFamily: {
         'sans': ['Open Sans', ...defaultTheme.fontFamily.sans],
         'display': ['Abril Fatface', 'Georgia', 'serif'],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.black'),
+          },
+        },
+      }),
     },
   },
   variants: {
     extend: {},
   },
   plugins: [
+    require('@tailwindcss/typography'),
   ]
 }
