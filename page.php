70cb2ad39cfbcba473b605591f1cee3b4ffd2a47
David Flindall <david@curiousways.com>
2021-07-01 13:28:03 +0100

    restructred pages
    Signed-off-by: David Flindall <david@curiousways.com>

--- page.php
+++ page.php
@@ -25,4 +25,11 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
+
+$args = array(
+  'numberposts' => 3,
+  'order' => 'DESC',
+);
+$context['latest_posts'] = Timber::get_posts($args);
+  
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
