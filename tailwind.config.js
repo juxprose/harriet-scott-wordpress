42a5eb061db3e7899fe7472c23f466410a9ad6b9
David Flindall <david@curiousways.com>
2021-07-01 08:49:11 +0100

    new layout for footer and about section
    Signed-off-by: David Flindall <david@curiousways.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: [
-    '**/*.twig',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-     colors: {
-       'hs-maroon': '#800000',
-      },
-      fontFamily: {
-        'sans': ['Open Sans', ...defaultTheme.fontFamily.sans],
-        'display': ['Abril Fatface', 'Georgia', 'serif'],
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.black'),
-          },
-        },
-      }),
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ]
-}
+	purge: ['**/*.twig'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				'hs-maroon': '#800000',
+				'hs-black': '#434343',
+				'hs-dust-pink': '#FFA082',
+				'hs-marmelade': '#ffddb0',
+				'hs-seagrove-50': '#d3f6f1',
+				'hs-seagrove': '#B6EBE3',
+			},
+			height: {
+				670: '670px',
+			},
+			fontFamily: {
+				sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+				display: ['Abril Fatface', 'Georgia', 'serif'],
+			},
+			typography: theme => ({
+				DEFAULT: {
+					css: {
+						color: theme('colors.hs-black'),
+					},
+				},
+			}),
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/typography')],
+};
