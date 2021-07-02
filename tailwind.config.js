51779d36edb7c94a3a0bf85cd444e3364d90f37f
David Flindall <david@curiousways.com>
2021-07-02 10:38:00 +0100

    single post
    Signed-off-by: David Flindall <david@curiousways.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,5 +33,5 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
